845a4999be8eac9421559880c2a29bf2d466cdfe
qahmed <ahmedqasimahmed@gmail.com>
2025-09-27 14:57:02 +0530

    first Commit

---
+++ src/test/java/Academy/BrowserTest.java
@@ -0,0 +1,35 @@
+package Academy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+
+public class BrowserTest {
+
+	
+	@Test
+	public void getData()
+	{
+		System.out.println("Hello Guys");
+		
+		WebDriverManager.chromedriver().setup();
+        WebDriver driver =new ChromeDriver();
+		driver.get("https://qasimahmed-a3f9g4beczand5a6.canadacentral-01.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		driver.close();
+	
+		
+		
+	}
+}
